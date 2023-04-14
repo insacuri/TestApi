6824595901bc09d550807316f4b0e08df851d18e
insacuri <manny160389@hotmail.com>
2023-04-14 11:57:03 +0100

    Build docker image with instructions

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # TestApi
 Test API written in C#
+
+## Docker
+
+In the root of the project
+
+```
+docker build -f TestApi/Dockerfile -t testapi .
+docker run -d -p 5000:80 --name testapi testapi
+```
--- TestApi/Controllers/WeatherForecastController.cs
+++ TestApi/Controllers/WeatherForecastController.cs
@@ -23,11 +23,11 @@ public class WeatherForecastController : ControllerBase
     {
         _logger.LogInformation("Getting the weather forecast");
         return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
             .ToArray();
     }
 
